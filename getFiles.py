2806b881f23200814c96c04937a6561f54d03ac2
Miles Peyton <milespeyton@gmail.com>
2015-05-14 22:39:37 -0400

    aonetutnaoeus

--- getFiles.py
+++ getFiles.py
@@ -1,9 +1,12 @@
 import os
 
-with open("textures.txt", "w") as texList:
+with open("temp.txt", "w") as texList:
     for dirpath, dirnames, files in os.walk('textures'):
         if dirpath == 'textures':
             for file in files:
                 if file[0] != '.':
                     print('writing ' + file)
                     texList.write(file + '\n')
+os.system('gshuf temp.txt > textures.txt')
+os.system('rm temp.txt')
+
