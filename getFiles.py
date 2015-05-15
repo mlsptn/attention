f1236bd2529b92ca4ecfd05d018ff48a1f995480
Miles Peyton <milespeyton@gmail.com>
2015-05-15 08:24:54 -0400

    a lot

--- getFiles.py
+++ getFiles.py
@@ -1,8 +1,8 @@
 import os
 
 with open("temp.txt", "w") as texList:
-    for dirpath, dirnames, files in os.walk('textures'):
-        if dirpath == 'textures':
+    for dirpath, dirnames, files in os.walk('optimized'):
+        if dirpath == 'optimized':
             for file in files:
                 if file[0] != '.':
                     print('writing ' + file)
